fc4d43b804e335dae3ed8ebc88ed9d750cb00798
Aliaksei Malochnikau <alexei.molochnikov@gmail.com>
2023-11-26 14:35:19 +0100

    Update Weasley last name

--- homework/pytest_homework/tests/test_pytest_homework.py
+++ homework/pytest_homework/tests/test_pytest_homework.py
@@ -4,7 +4,7 @@ import pytest
 class TestHomework:
     @pytest.mark.wizard
     @pytest.mark.parametrize("first_name", ["John", "Harry", "Ron"])
-    @pytest.mark.parametrize("last_name", ["Smith", "Potter", "Weasly"])
+    @pytest.mark.parametrize("last_name", ["Smith", "Potter", "Weasley"])
     def test_fail_if_john_smith(self, age, first_name, last_name):
         if first_name == "John" and last_name == "Smith":
             print("John Smith? Seriously?")
